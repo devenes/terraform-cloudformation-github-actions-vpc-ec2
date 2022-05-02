579a71cd38f1cd7624890977e8e400f29a9f1695
Enes <66560757+devenes@users.noreply.github.com>
2022-05-03 02:26:39 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
 terraform {
   required_version = ">= 0.12.0"
 
-  # backend "s3" {
-  #   bucket = "terraform-env-bucket"
-  #   key    = "key/terraform.tfstate"
-  #   region = "us-east-1"
-  # }
+  backend "s3" {
+    bucket = "terraform-env-bucket"
+    key    = "key/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
-  region  = "us-east-1"
-  profile = "default"
+  region = "us-east-1"
+  # profile = "default"
 }
